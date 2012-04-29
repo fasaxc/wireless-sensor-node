8a1ec52f79c3e8e4dd091806b4c4b6e0a33c510a
Shaun Crampton <Shaun.Crampton@metaswitch.com>
2012-04-29 10:53:58 -0700

    Implement reading of temperature sensor.

--- main.c
+++ main.c
@@ -2,23 +2,83 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "sensor_node.h"
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
 
 #define TX_PORT PORTB
+#define TX_TOGGLE_REG PINB
 #define TX_PIN PB3
+
+#define TEMP_SENSE_POWER_PORT PORTB
+#define TEMP_SENSE_POWER_PIN PB0
+
+#define TEMP_SENSE_INPUT_DIG_PIN PB4
+#define TEMP_SENSE_INPUT_AN_PIN 2
 
 void transmit(void);
 
 #define HALF_BIT_DELAY_TIME_MILLIS 0.5
 
+static volatile char adc_done;
+
+ISR(ADC_vect)
+{
+    adc_done = 1;
+}
+
+inline static void do_adc_conversion() {
+    adc_done = 0;
+    set_sleep_mode(SLEEP_MODE_ADC);
+    sleep_mode();
+    while (!adc_done) {
+        // Just in case we were woken by a different interrupt.
+    }
+}
+
+inline static uint16_t read_temperature() {
+    // Power up the temperature sensor.
+    TEMP_SENSE_POWER_PORT |= _BV(TEMP_SENSE_POWER_PIN);
+
+    // Configure the ADC.
+    ADMUX = _BV(REFS0) | /* 1.1V reference */
+            TEMP_SENSE_INPUT_AN_PIN /* Pin selection */;
+    ADCSRA = _BV(ADEN) | /* Enable ADC */
+             _BV(ADIE) | /* Enable completion interrupt. */
+             6           /* Select CLK/32 prescaler. */;
+
+    // Trigger a conversion but discard the result because the first conversion
+    // can be garbage.
+    do_adc_conversion();
+
+    // Use the second conversion.  Kick it off by entering sleep mode.
+    do_adc_conversion();
+    uint16_t result = ADCW;
+
+    // Disable ADC.
+    ADCSRA &= ~_BV(ADEN);
+
+    // Power down the temp sensor.
+    TEMP_SENSE_POWER_PORT &= ~_BV(TEMP_SENSE_POWER_PIN);
+
+    return result;
+}
+
 int main(void)
 {
-    DDRB = 255;
+    // Configure all pins as outputs except the temperature input pin.
+    DDRB = 0xFF ^ _BV(TEMP_SENSE_INPUT_DIG_PIN);
+    // Disable digital input buffer on the analog input pin.
+    DIDR0 |= _BV(TEMP_SENSE_INPUT_DIG_PIN);
+
+    // Enable interrupts.
+    sei();
+
     while (1)
     {
         manchester_union.manchester_packet.node_id = 1;
         manchester_union.manchester_packet.seq_no += 1;
         manchester_union.manchester_packet.reading_type = READING_TYPE_TEMP;
-        manchester_union.manchester_packet.reading = 0xBEEF;
+        manchester_union.manchester_packet.reading = read_temperature();
         transmit();
         _delay_ms(500);
     }
